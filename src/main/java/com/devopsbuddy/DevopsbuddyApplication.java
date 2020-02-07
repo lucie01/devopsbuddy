ecccef7c16b7bed888439fdf8e9972fc83643195
Luciene Ceh Cunha <luciene.cunha.ext@spms.min-saude.pt>
2020-02-07 11:48:37 +0000

    Initial commit to our first Spring Boot project

---
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -0,0 +1,14 @@
+package com.devopsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsbuddyApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(DevopsbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -0,0 +1,5 @@
+package devopsbuddy;
+
+public class DevopsbuddyApplicationTests {
+
+}

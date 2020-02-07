43cb9f3944e9f44cda30c2b764ac4e501cd8b9ad
Luciene Ceh Cunha <luciene.cunha.ext@spms.min-saude.pt>
2020-02-07 23:32:39 +0000

    Adding a new controller and a new view

---
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -0,0 +1,11 @@
+package com.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+    public String home() {
+        return "intex";
+    }
+}

735e1c504a4a7c4062a705eb9364997187afee22
Luciene Ceh Cunha <luciene.cunha.ext@spms.min-saude.pt>
2020-02-08 16:14:01 +0000

    Added i18n capabilyties to the web skeleton

---
+++ src/main/java/com/devopsbuddy/config/i18n/I18NConfig.java
@@ -0,0 +1,29 @@
+package com.devopsbuddy.config.i18n;
+
+import com.devopsbuddy.web.i18n.I18NService;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class I18NConfig {
+
+    //To spring starts the message source correctly its necessary that the bean name as the following code
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource reloadableResourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
+        reloadableResourceBundleMessageSource.setBasename("classpath:i18n/messages");
+        // Checks for new messages every 30 minutes
+        reloadableResourceBundleMessageSource.setCacheSeconds(1800);
+        return reloadableResourceBundleMessageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
--- src/test/java/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,5 +1,32 @@
 package devopsbuddy;
 
+import com.devopsbuddy.config.i18n.I18NConfig;
+import com.devopsbuddy.web.i18n.I18NService;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {I18NService.class, I18NConfig.class})
 public class DevopsbuddyApplicationTests {
 
+    @Autowired
+    private I18NService i18NService;
+
+    @Test
+    public void testMessageByLocaleService() throws Exception {
+        String expectedResult = "Bootstrap starter template";
+        String messageId = "index.main.callout";
+        String resultMessage = i18NService.getMessage(messageId);
+        Assert.assertEquals("The actual and spected Strings don't match", expectedResult,resultMessage);
+    }
 }

e1ac89adcf512cb85b418244d8a203b669b6c834
Luciene Ceh Cunha <luciene.cunha.ext@spms.min-saude.pt>
2020-02-12 19:19:41 +0000

    Addedsystem administrator login and email details

--- src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -9,6 +9,7 @@ import com.devopsbuddy.enums.RolesEnum;
 import com.devopsbuddy.utils.UserUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,6 +24,12 @@ public class DevopsbuddyApplication implements CommandLineRunner {
 
 	@Autowired
 	private UserService userService;
+	@Value("${sysadmin.username}")
+	private String sysAdminUsername;
+	@Value("${sysadmin.password}")
+	private String sysAdminPassword;
+	@Value("${sysadmin.email}")
+	private String sysAdminEmail;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -32,10 +39,8 @@ public class DevopsbuddyApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		String username = "proUser";
-		String email = "cunhalucie01@gmail.com";
-		User basicUser = UserUtils.createBasicUser(username, email);
-
+		User basicUser = UserUtils.createBasicUser(sysAdminUsername, sysAdminEmail);
+		basicUser.setPassword(sysAdminPassword);
 		log.debug("Creating user with username {}", basicUser.getUsername());
 
 		User user = userService.createUser(basicUser, PlansEnum.PRO, RolesEnum.PRO);
